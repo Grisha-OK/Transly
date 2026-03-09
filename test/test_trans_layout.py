21225cdd1730e1920483a6011d8b05c1b1ca3ce4
grushaok <rotanov_g@mail.ru>
2026-03-09 22:33:52 +0400

    Add requirement files and prepare test_trans_layout.py for writing unit tests

--- test/test_trans_layout.py
+++ test/test_trans_layout.py
@@ -1,5 +1,8 @@
 from Transly import TransLayout, SwitchState
+<<<<<<< HEAD
 import clipboard
+=======
+>>>>>>> f4100de (Add requirement files and prepare test_trans_layout.py for writing unit tests)
 
 AIL_ALPHABET_LANGUAGE = {
     "ё": "`", "Ё": "~", "й": "q", "Й": "Q", "ц": "w", "Ц": "W", "у": "e", "У": "E", "к": "r", "К": "R", "е": "t", "Е": "T", "н": "y", "Н": "Y", "г": "u", "Г": "U", "ш": "i",
@@ -27,6 +30,7 @@ fake_tjson = type('TransJson', (object,), {
     "hot_key_n2": hot_key_n2, 
     "dictionary": dictionary, 
     "baf_state": fake_swof
+<<<<<<< HEAD
 })()
 
 
@@ -42,3 +46,6 @@ def test_master_keyboard_worker_with_hotkey():
 def test_master_transly_worker():
     tl = TransLayout(fake_tjson)
     assert tl.master_transly_worker("Hello World", False) == "Привет, мир"
+=======
+})()
+>>>>>>> f4100de (Add requirement files and prepare test_trans_layout.py for writing unit tests)
