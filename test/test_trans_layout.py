5524129e30dc3a9a1991b93d70f594c63778d87f
grushaok <rotanov_g@mail.ru>
2026-03-11 17:01:37 +0400

    Add language constants for English and Russian in test_trans_layout.py

--- test/test_trans_layout.py
+++ test/test_trans_layout.py
@@ -7,12 +7,24 @@ AIL_ALPHABET_LANGUAGE = {
 dictionary = AIL_ALPHABET_LANGUAGE
 
 # ... переменные HOT_KEY и fake_swof ...
+HOT_KEY_LAYOUT = ("ctrl + F9")
+hot_key_n1 = HOT_KEY_LAYOUT
+
+HOT_KEY_TRANSLATION = ("ctrl + F8")
+hot_key_n2 = HOT_KEY_TRANSLATION
+
+LANGUAGE_EN = ("en")
+LANGUAGE_RU = ("ru")
+
+fake_swof = SwitchState()
 
 fake_tjson = type('TransJson', (object,), {
     "hot_key_n1": hot_key_n1, 
     "hot_key_n2": hot_key_n2, 
     "dictionary": dictionary, 
-    "baf_state": fake_swof
+    "baf_state": fake_swof,
+    "language_1" : LANGUAGE_EN,
+    "language_2" : LANGUAGE_RU
 })()
 
 # Оставляем эти тесты, которые были в HEAD
